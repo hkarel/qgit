ea528b73df40f7dd15b7bdc3a0695ddca0457b71
Pavel Karelin <hkarel@yandex.ru>
2021-01-21 12:00:04 +0300

    Refactoring: Changed GUI-component names for Settings form

--- src/settingsimpl.cpp
+++ src/settingsimpl.cpp
@@ -47,37 +47,37 @@ SettingsImpl::SettingsImpl(QWidget* p, Git* g, int defTab) : QDialog(p), git(g) 
     setupUi(this);
 
 //    int f = flags(FLAGS_KEY);
-//    checkBoxDiffCache->setChecked(f & DIFF_INDEX_F);
-//    checkBoxNumbers->setChecked(f & NUMBERS_F);
-//    checkBoxSign->setChecked(f & SIGN_PATCH_F);
-//    checkBoxCommitSign->setChecked(f & SIGN_CMT_F);
-//    checkBoxCommitVerify->setChecked(f & VERIFY_CMT_F);
-//    checkBoxCommitUseDefMsg->setChecked(f & USE_CMT_MSG_F);
-//    checkBoxRangeSelectDialog->setChecked(f & RANGE_SELECT_F);
-//    checkBoxReopenLastRepo->setChecked(f & REOPEN_REPO_F);
-//    checkBoxOpenInEditor->setChecked(f & OPEN_IN_EDITOR_F);
-//    checkBoxRelativeDate->setChecked(f & REL_DATE_F);
-//    checkBoxLogDiffTab->setChecked(f & LOG_DIFF_TAB_F);
-//    checkBoxSmartLabels->setChecked(f & SMART_LBL_F);
-//    checkBoxMsgOnNewSHA->setChecked(f & MSG_ON_NEW_F);
-//    checkBoxEnableDragnDrop->setChecked(f & ENABLE_DRAGNDROP_F);
+//    chkDiffCache->setChecked(f & DIFF_INDEX_F);
+//    chkNumbers->setChecked(f & NUMBERS_F);
+//    chkSign->setChecked(f & SIGN_PATCH_F);
+//    chkCommitSign->setChecked(f & SIGN_CMT_F);
+//    chkCommitVerify->setChecked(f & VERIFY_CMT_F);
+//    chkCommitUseDefMsg->setChecked(f & USE_CMT_MSG_F);
+//    chkRangeSelectDialog->setChecked(f & RANGE_SELECT_F);
+//    chkReopenLastRepo->setChecked(f & REOPEN_REPO_F);
+//    chkOpenInEditor->setChecked(f & OPEN_IN_EDITOR_F);
+//    chkRelativeDate->setChecked(f & REL_DATE_F);
+//    chkLogDiffTab->setChecked(f & LOG_DIFF_TAB_F);
+//    chkSmartLabels->setChecked(f & SMART_LBL_F);
+//    chkMsgOnNewSHA->setChecked(f & MSG_ON_NEW_F);
+//    chkEnableDragnDrop->setChecked(f & ENABLE_DRAGNDROP_F);
 
     qgit::FlagType f = qgit::flags();
 
-    checkBoxDiffCache->setChecked(f & DIFF_INDEX_F);
-    checkBoxNumbers->setChecked(f & NUMBERS_F);
-    checkBoxSign->setChecked(f & SIGN_PATCH_F);
-    checkBoxCommitSign->setChecked(f & SIGN_CMT_F);
-    checkBoxCommitVerify->setChecked(f & VERIFY_CMT_F);
-    checkBoxCommitUseDefMsg->setChecked(f & USE_CMT_MSG_F);
-    checkBoxRangeSelectDialog->setChecked(f & RANGE_SELECT_F);
-    checkBoxReopenLastRepo->setChecked(f & REOPEN_REPO_F);
-    checkBoxOpenInEditor->setChecked(f & OPEN_IN_EDITOR_F);
-    checkBoxRelativeDate->setChecked(f & REL_DATE_F);
-    checkBoxLogDiffTab->setChecked(f & LOG_DIFF_TAB_F);
-    checkBoxSmartLabels->setChecked(f & SMART_LBL_F);
-    checkBoxMsgOnNewSHA->setChecked(f & MSG_ON_NEW_F);
-    checkBoxEnableDragnDrop->setChecked(f & ENABLE_DRAGNDROP_F);
+    chkDiffCache->setChecked(f & DIFF_INDEX_F);
+    chkNumbers->setChecked(f & NUMBERS_F);
+    chkSign->setChecked(f & SIGN_PATCH_F);
+    chkCommitSign->setChecked(f & SIGN_CMT_F);
+    chkCommitVerify->setChecked(f & VERIFY_CMT_F);
+    chkCommitUseDefMsg->setChecked(f & USE_CMT_MSG_F);
+    chkRangeSelectDialog->setChecked(f & RANGE_SELECT_F);
+    chkReopenLastRepo->setChecked(f & REOPEN_REPO_F);
+    chkOpenInEditor->setChecked(f & OPEN_IN_EDITOR_F);
+    chkRelativeDate->setChecked(f & REL_DATE_F);
+    chkLogDiffTab->setChecked(f & LOG_DIFF_TAB_F);
+    chkSmartLabels->setChecked(f & SMART_LBL_F);
+    chkMsgOnNewSHA->setChecked(f & MSG_ON_NEW_F);
+    chkEnableDragnDrop->setChecked(f & ENABLE_DRAGNDROP_F);
 
     QString FPOpt;
     config::base().getValue("patch.args", FPOpt);
@@ -106,24 +106,24 @@ SettingsImpl::SettingsImpl(QWidget* p, Git* g, int defTab) : QDialog(p), git(g) 
     QString commitTmpl;
     config::base().getValue("commit.template_file_path", commitTmpl);
 
-    lineEditApplyPatchExtraOptions->setText(APOpt);
-    lineEditFormatPatchExtraOptions->setText(FPOpt);
-    lineEditExternalDiffViewer->setText(extDiff);
-    lineEditExternalEditor->setText(extEditor);
-    lineEditExcludeFile->setText(exFile);
-    lineEditExcludePerDir->setText(exPerDir);
-    lineEditTemplate->setText(commitTmpl);
-    lineEditCommitExtraOptions->setText(CMArgs);
-    lineEditTypeWriterFont->setText(TYPE_WRITER_FONT.toString());
-    lineEditTypeWriterFont->setCursorPosition(0); // font description could be long
+    lineApplyPatchExtraOptions->setText(APOpt);
+    lineFormatPatchExtraOptions->setText(FPOpt);
+    lineExternalDiffViewer->setText(extDiff);
+    lineExternalEditor->setText(extEditor);
+    lineExcludeFile->setText(exFile);
+    lineExcludePerDir->setText(exPerDir);
+    lineTemplate->setText(commitTmpl);
+    lineCommitExtraOptions->setText(CMArgs);
+    lineTypeWriterFont->setText(TYPE_WRITER_FONT.toString());
+    lineTypeWriterFont->setCursorPosition(0); // font description could be long
 
-    comboBoxIconSize->setCurrentIndex(iconSizeIndex);
+    cboxIconSize->setCurrentIndex(iconSizeIndex);
 
     setupCodecsCombo();
-    checkBoxDiffCache_toggled(checkBoxDiffCache->isChecked());
+    on_chkDiffCache_toggled(chkDiffCache->isChecked());
     tabDialog->setCurrentIndex(defTab);
     userInfo();
-    comboBoxGitConfigSource_activated(0);
+    on_cboxGitConfigSource_activated(0);
 
     loadGeometry();
 }
@@ -159,7 +159,7 @@ void SettingsImpl::userInfo() {
     bool found = false;
     int idx = 0;
     FOREACH(it, _uInfo) {
-        comboBoxUserSrc->addItem(*it);
+        cboxUserSrc->addItem(*it);
         ++it;
         if (!found && !(*it).isEmpty())
             found = true;
@@ -170,8 +170,8 @@ void SettingsImpl::userInfo() {
     if (!found)
         idx = 0;
 
-    comboBoxUserSrc->setCurrentIndex(idx);
-    comboBoxUserSrc_activated(idx);
+    cboxUserSrc->setCurrentIndex(idx);
+    on_cboxUserSrc_activated(idx);
 }
 
 void SettingsImpl::addConfigOption(QTreeWidgetItem* parent, QStringList paths, const QString& value) {
@@ -193,7 +193,7 @@ void SettingsImpl::addConfigOption(QTreeWidgetItem* parent, QStringList paths, c
 void SettingsImpl::readGitConfig(const QString& source) {
  
     populatingGitConfig = true;
-    treeWidgetGitConfig->clear();
+    treeGitConfig->clear();
     QStringList options = git->getGitConfigList(source == "Global");
     options.sort();
 
@@ -208,11 +208,11 @@ void SettingsImpl::readGitConfig(const QString& source) {
         }
         QString name(paths.first());
         paths.removeFirst();
-        QList<QTreeWidgetItem*> items = treeWidgetGitConfig->findItems(name, Qt::MatchExactly);
+        QList<QTreeWidgetItem*> items = treeGitConfig->findItems(name, Qt::MatchExactly);
         QTreeWidgetItem* item;
 
         if (items.isEmpty())
-            item = new QTreeWidgetItem(treeWidgetGitConfig, QStringList(name));
+            item = new QTreeWidgetItem(treeGitConfig, QStringList(name));
         else
             item = items.first();
 
@@ -221,7 +221,7 @@ void SettingsImpl::readGitConfig(const QString& source) {
     populatingGitConfig = false;
 }
 
-void SettingsImpl::treeWidgetGitConfig_itemChanged(QTreeWidgetItem* item, int i) {
+void SettingsImpl::on_treeGitConfig_itemChanged(QTreeWidgetItem* item, int i) {
 
     if (populatingGitConfig)
         return;
@@ -230,15 +230,15 @@ void SettingsImpl::treeWidgetGitConfig_itemChanged(QTreeWidgetItem* item, int i)
     //dbp("column %1", i);
 }
 
-void SettingsImpl::comboBoxUserSrc_activated(int i) {
+void SettingsImpl::on_cboxUserSrc_activated(int i) {
 
-    lineEditAuthor->setText(_uInfo[i * 3 + 1]);
-    lineEditMail->setText(_uInfo[i * 3 + 2]);
+    lineAuthor->setText(_uInfo[i * 3 + 1]);
+    lineMail->setText(_uInfo[i * 3 + 2]);
 }
 
-void SettingsImpl::comboBoxGitConfigSource_activated(int) {
+void SettingsImpl::on_cboxGitConfigSource_activated(int) {
 
-    readGitConfig(comboBoxGitConfigSource->currentText());
+    readGitConfig(cboxGitConfigSource->currentText());
 }
 
 void SettingsImpl::setupCodecList(QStringList& list) {
@@ -254,12 +254,12 @@ void SettingsImpl::setupCodecsCombo() {
     QStringList codecs;
     codecs.append(QString("Local Codec (" + localCodec + ")"));
     setupCodecList(codecs);
-    comboBoxCodecs->insertItems(0, codecs);
+    cboxCodecs->insertItems(0, codecs);
 
     bool isGitArchive;
     QTextCodec* tc = git->getTextCodec(&isGitArchive);
     if (!isGitArchive) {
-        comboBoxCodecs->setEnabled(false);
+        cboxCodecs->setEnabled(false);
         return;
     }
     const QString curCodec(tc != 0 ? tc->name() : "Latin1");
@@ -269,51 +269,51 @@ void SettingsImpl::setupCodecsCombo() {
         log_warn << log_format("Codec <%?> not available, using local codec", curCodec);
         idx = 0;
     }
-    comboBoxCodecs->setCurrentIndex(idx);
+    cboxCodecs->setCurrentIndex(idx);
     if (idx == 0) // signal activated() will not fire in this case
-        comboBoxCodecs_activated(0);
+        on_cboxCodecs_activated(0);
 }
 
-void SettingsImpl::comboBoxIconSize_activated(int i) {
+void SettingsImpl::on_cboxIconSize_activated(int i) {
 
     //writeSetting(ICON_SIZE_INDEX, i);
     config::base().setValue("general.icon_size_index", i);
 }
 
-void SettingsImpl::comboBoxCodecs_activated(int idx) {
+void SettingsImpl::on_cboxCodecs_activated(int idx) {
 
     QString codecName(QTextCodec::codecForLocale()->name());
     if (idx != 0)
-        codecName = comboBoxCodecs->currentText().section(" --", 0, 0);
+        codecName = cboxCodecs->currentText().section(" --", 0, 0);
 
     git->setTextCodec(QTextCodec::codecForName(codecName.toLatin1()));
 }
 
-void SettingsImpl::pushButtonExtDiff_clicked() {
+void SettingsImpl::on_btnExtDiff_clicked() {
 
     QString extDiffName(QFileDialog::getOpenFileName(this,
                         "Select the patch viewer"));
     if (!extDiffName.isEmpty())
-        lineEditExternalDiffViewer->setText(extDiffName);
+        lineExternalDiffViewer->setText(extDiffName);
 }
 
-void SettingsImpl::pushButtonExtEditor_clicked() {
+void SettingsImpl::on_btnExtEditor_clicked() {
 
     QString extEditorName(QFileDialog::getOpenFileName(this,
                         "Select the external editor"));
     if (!extEditorName.isEmpty())
-        lineEditExternalEditor->setText(extEditorName);
+        lineExternalEditor->setText(extEditorName);
 }
 
-void SettingsImpl::pushButtonFont_clicked() {
+void SettingsImpl::on_btnFont_clicked() {
 
     bool ok;
     QFont fnt = QFontDialog::getFont(&ok, TYPE_WRITER_FONT, this);
     if (ok && TYPE_WRITER_FONT != fnt) {
 
         TYPE_WRITER_FONT = fnt;
-        lineEditTypeWriterFont->setText(fnt.toString());
-        lineEditTypeWriterFont->setCursorPosition(0);
+        lineTypeWriterFont->setText(fnt.toString());
+        lineTypeWriterFont->setCursorPosition(0);
 
         //writeSetting(TYPWRT_FNT_KEY, fnt.toString());
         config::base().setValue("general.typewriter_font", fnt.toString());
@@ -340,114 +340,114 @@ void SettingsImpl::done(int r)
     saveGeometry();
 }
 
-void SettingsImpl::checkBoxDiffCache_toggled(bool b) {
+void SettingsImpl::on_chkDiffCache_toggled(bool b) {
 
-    lineEditExcludeFile->setEnabled(b);
-    lineEditExcludePerDir->setEnabled(b);
+    lineExcludeFile->setEnabled(b);
+    lineExcludePerDir->setEnabled(b);
     changeFlag(DIFF_INDEX_F, b);
 }
 
-void SettingsImpl::checkBoxNumbers_toggled(bool b) {
+void SettingsImpl::on_chkNumbers_toggled(bool b) {
 
     changeFlag(NUMBERS_F, b);
 }
 
-void SettingsImpl::checkBoxSign_toggled(bool b) {
+void SettingsImpl::on_chkSign_toggled(bool b) {
 
     changeFlag(SIGN_PATCH_F, b);
 }
 
-void SettingsImpl::checkBoxRangeSelectDialog_toggled(bool b) {
+void SettingsImpl::on_chkRangeSelectDialog_toggled(bool b) {
 
     changeFlag(RANGE_SELECT_F, b);
 }
 
-void SettingsImpl::checkBoxReopenLastRepo_toggled(bool b) {
+void SettingsImpl::on_chkReopenLastRepo_toggled(bool b) {
 
     changeFlag(REOPEN_REPO_F, b);
 }
 
-void SettingsImpl::checkBoxOpenInEditor_toggled(bool b) {
+void SettingsImpl::on_chkOpenInEditor_toggled(bool b) {
 
     changeFlag(OPEN_IN_EDITOR_F, b);
 }
 
-void SettingsImpl::checkBoxRelativeDate_toggled(bool b) {
+void SettingsImpl::on_chkRelativeDate_toggled(bool b) {
 
     changeFlag(REL_DATE_F, b);
 }
 
-void SettingsImpl::checkBoxLogDiffTab_toggled(bool b) {
+void SettingsImpl::on_chkLogDiffTab_toggled(bool b) {
 
     changeFlag(LOG_DIFF_TAB_F, b);
 }
 
-void SettingsImpl::checkBoxSmartLabels_toggled(bool b) {
+void SettingsImpl::on_chkSmartLabels_toggled(bool b) {
 
     changeFlag(SMART_LBL_F, b);
 }
 
-void SettingsImpl::checkBoxMsgOnNewSHA_toggled(bool b) {
+void SettingsImpl::on_chkMsgOnNewSHA_toggled(bool b) {
 
     changeFlag(MSG_ON_NEW_F, b);
 }
 
-void SettingsImpl::checkBoxEnableDragnDrop_toggled(bool b) {
+void SettingsImpl::on_chkEnableDragnDrop_toggled(bool b) {
 
 	changeFlag(ENABLE_DRAGNDROP_F, b);
 }
 
-void SettingsImpl::checkBoxCommitSign_toggled(bool b) {
+void SettingsImpl::on_chkCommitSign_toggled(bool b) {
 
     changeFlag(SIGN_CMT_F, b);
 }
 
-void SettingsImpl::checkBoxCommitVerify_toggled(bool b) {
+void SettingsImpl::on_chkCommitVerify_toggled(bool b) {
 
     changeFlag(VERIFY_CMT_F, b);
 }
 
-void SettingsImpl::checkBoxCommitUseDefMsg_toggled(bool b) {
+void SettingsImpl::on_chkCommitUseDefMsg_toggled(bool b) {
 
     changeFlag(USE_CMT_MSG_F, b);
 }
 
-void SettingsImpl::lineEditExternalDiffViewer_textChanged(const QString& s) {
+void SettingsImpl::on_lineExternalDiffViewer_textChanged(const QString& s) {
 
     config::base().setValue("general.external_diff_viewer", s);
 }
 
-void SettingsImpl::lineEditExternalEditor_textChanged(const QString& s) {
+void SettingsImpl::on_lineExternalEditor_textChanged(const QString& s) {
 
     config::base().setValue("general.external_editor", s);
 }
 
-void SettingsImpl::lineEditApplyPatchExtraOptions_textChanged(const QString& s) {
+void SettingsImpl::on_lineApplyPatchExtraOptions_textChanged(const QString& s) {
 
     config::base().setValue("patch.args_2", s);
 }
 
-void SettingsImpl::lineEditFormatPatchExtraOptions_textChanged(const QString& s) {
+void SettingsImpl::on_lineFormatPatchExtraOptions_textChanged(const QString& s) {
 
     config::base().setValue("patch.args", s);
 }
 
-void SettingsImpl::lineEditExcludeFile_textChanged(const QString& s) {
+void SettingsImpl::on_lineExcludeFile_textChanged(const QString& s) {
 
     config::base().setValue("working_dir.exclude.from", s);
 }
 
-void SettingsImpl::lineEditExcludePerDir_textChanged(const QString& s) {
+void SettingsImpl::on_lineExcludePerDir_textChanged(const QString& s) {
 
     config::base().setValue("working_dir.exclude.per_directory", s);
 }
 
-void SettingsImpl::lineEditTemplate_textChanged(const QString& s) {
+void SettingsImpl::on_lineTemplate_textChanged(const QString& s) {
 
     config::base().setValue("commit.template_file_path", s);
 }
 
-void SettingsImpl::lineEditCommitExtraOptions_textChanged(const QString& s) {
+void SettingsImpl::on_lineCommitExtraOptions_textChanged(const QString& s) {
 
     config::base().setValue("commit/args", s);
 }
--- src/settingsimpl.h
+++ src/settingsimpl.h
@@ -12,7 +12,7 @@
 class QVariant;
 class Git;
 
-class SettingsImpl: public QDialog, public Ui_settingsBase {
+class SettingsImpl: public QDialog, public Ui_SettingsBase {
 Q_OBJECT
 public:
     SettingsImpl(QWidget* parent, Git* git, int defTab = 0);
@@ -25,36 +25,36 @@ signals:
     void flagChanged(uint);
 
 protected slots:
-    void checkBoxNumbers_toggled(bool b);
-    void checkBoxSign_toggled(bool b);
-    void checkBoxRangeSelectDialog_toggled(bool b);
-    void checkBoxReopenLastRepo_toggled(bool b);
-    void checkBoxOpenInEditor_toggled(bool b);
-    void checkBoxRelativeDate_toggled(bool b);
-    void checkBoxLogDiffTab_toggled(bool b);
-    void checkBoxSmartLabels_toggled(bool b);
-    void checkBoxMsgOnNewSHA_toggled(bool b);
-	void checkBoxEnableDragnDrop_toggled(bool b);
-    void checkBoxDiffCache_toggled(bool b);
-    void checkBoxCommitSign_toggled(bool b);
-    void checkBoxCommitVerify_toggled(bool b);
-    void checkBoxCommitUseDefMsg_toggled(bool b);
-    void lineEditExternalDiffViewer_textChanged(const QString& s);
-    void lineEditExternalEditor_textChanged(const QString& s);
-    void lineEditApplyPatchExtraOptions_textChanged(const QString& s);
-    void lineEditFormatPatchExtraOptions_textChanged(const QString& s);
-    void lineEditExcludeFile_textChanged(const QString& s);
-    void lineEditExcludePerDir_textChanged(const QString& s);
-    void lineEditTemplate_textChanged(const QString& s);
-    void lineEditCommitExtraOptions_textChanged(const QString& s);
-    void comboBoxIconSize_activated(int i);
-    void comboBoxCodecs_activated(int i);
-    void comboBoxUserSrc_activated(int i);
-    void comboBoxGitConfigSource_activated(int i);
-    void treeWidgetGitConfig_itemChanged(QTreeWidgetItem*, int);
-    void pushButtonExtDiff_clicked();
-    void pushButtonExtEditor_clicked();
-    void pushButtonFont_clicked();
+    void on_chkNumbers_toggled(bool b);
+    void on_chkSign_toggled(bool b);
+    void on_chkRangeSelectDialog_toggled(bool b);
+    void on_chkReopenLastRepo_toggled(bool b);
+    void on_chkOpenInEditor_toggled(bool b);
+    void on_chkRelativeDate_toggled(bool b);
+    void on_chkLogDiffTab_toggled(bool b);
+    void on_chkSmartLabels_toggled(bool b);
+    void on_chkMsgOnNewSHA_toggled(bool b);
+    void on_chkEnableDragnDrop_toggled(bool b);
+    void on_chkDiffCache_toggled(bool b);
+    void on_chkCommitSign_toggled(bool b);
+    void on_chkCommitVerify_toggled(bool b);
+    void on_chkCommitUseDefMsg_toggled(bool b);
+    void on_lineExternalDiffViewer_textChanged(const QString& s);
+    void on_lineExternalEditor_textChanged(const QString& s);
+    void on_lineApplyPatchExtraOptions_textChanged(const QString& s);
+    void on_lineFormatPatchExtraOptions_textChanged(const QString& s);
+    void on_lineExcludeFile_textChanged(const QString& s);
+    void on_lineExcludePerDir_textChanged(const QString& s);
+    void on_lineTemplate_textChanged(const QString& s);
+    void on_lineCommitExtraOptions_textChanged(const QString& s);
+    void on_cboxIconSize_activated(int i);
+    void on_cboxCodecs_activated(int i);
+    void on_cboxUserSrc_activated(int i);
+    void on_cboxGitConfigSource_activated(int i);
+    void on_treeGitConfig_itemChanged(QTreeWidgetItem*, int);
+    void on_btnExtDiff_clicked();
+    void on_btnExtEditor_clicked();
+    void on_btnFont_clicked();
 
 private:
     void done(int r) override;
