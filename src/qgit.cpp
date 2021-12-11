0081d5d96fb78efa8f03a2a6336dfde42c7754a3
Pavel Karelin <hkarel@yandex.ru>
2021-12-11 19:23:52 +0300

    Comment unused code

--- src/qgit.cpp
+++ src/qgit.cpp
@@ -114,29 +114,29 @@ int main(int argc, char* argv[])
     QCoreApplication::setOrganizationName("qgit");
     QCoreApplication::setApplicationName("qgit");
 
-	QCommandLineParser parser;
-	QCoreApplication::setApplicationVersion(PACKAGE_VERSION);
-	parser.addHelpOption();
-	parser.addVersionOption();
-	parser.setApplicationDescription("QGit, a Git GUI viewer");
-	parser.addPositionalArgument("git-log-args",
-		"Arguments forwarded to \"git log\"; for example:\n"
-		"   qgit --no-merges\n"
-		"   qgit v2.6.18.. include/scsi \\\n"
-		"                  drivers/scsi\n"
-		"   qgit --since=\"2 weeks ago\" -- kernel/\n"
-		"   qgit -r --name-status release..test\n"
-		"See \"man git-log\" for details.",
-		"[git-log-args]");
+//	QCommandLineParser parser;
+//	QCoreApplication::setApplicationVersion(PACKAGE_VERSION);
+//	parser.addHelpOption();
+//	parser.addVersionOption();
+//	parser.setApplicationDescription("QGit, a Git GUI viewer");
+//	parser.addPositionalArgument("git-log-args",
+//		"Arguments forwarded to \"git log\"; for example:\n"
+//		"   qgit --no-merges\n"
+//		"   qgit v2.6.18.. include/scsi \\\n"
+//		"                  drivers/scsi\n"
+//		"   qgit --since=\"2 weeks ago\" -- kernel/\n"
+//		"   qgit -r --name-status release..test\n"
+//		"See \"man git-log\" for details.",
+//		"[git-log-args]");
 
-	parser.parse(app.arguments());
+//	parser.parse(app.arguments());
 
-	if (parser.isSet("help")
-			|| parser.isSet("help-all")
-			|| parser.isSet("version")) {
-		QCoreApplication::setApplicationName("QGit");
-		parser.process(app.arguments());  // exits the process
-	}
+//	if (parser.isSet("help")
+//			|| parser.isSet("help-all")
+//			|| parser.isSet("version")) {
+//		QCoreApplication::setApplicationName("qgit");
+//		parser.process(app.arguments());  // exits the process
+//	}
 
     /* On Windows msysgit exec directory is set up
      * during installation so to always find git.exe
